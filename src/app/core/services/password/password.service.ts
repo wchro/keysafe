4daa124f4f81442c14336c9ae0e57cd188956d27
wchro <93561159+wchro@users.noreply.github.com>
2024-09-15 18:09:35 +0200

    Improvements

--- src/app/core/services/password/password.service.ts
+++ src/app/core/services/password/password.service.ts
@@ -8,6 +8,11 @@ import PasswordGenerator from '@smakss/password-generator';
 export class PasswordService {
   constructor(private api: ApiService) {}
 
+  addPassword(name: string, account: string, password: string, site: string) {
+    const body = { name, account, password, site };
+    return this.api.post('/passwords', body);
+  }
+
   getPasswords() {
     return this.api.get('/passwords');
   }
--- src/app/features/add/add.component.ts
+++ src/app/features/add/add.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from '../../core/services/api/api.service';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { PasswordService } from '../../core/services/password/password.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, LoaderComponent],
   templateUrl: './add.component.html',
-  styleUrl: './add.component.css'
+  styleUrl: './add.component.css',
 })
 export class AddComponent {
+  isLoading: boolean = false;
 
+  constructor(
+    private api: ApiService,
+    private passwordService: PasswordService,
+    private router: Router
+  ) {}
+
+  addForm = new FormGroup({
+    name: new FormControl(),
+    account: new FormControl(),
+    password: new FormControl(),
+    site: new FormControl(),
+  });
+
+  onSubmit() {
+    this.isLoading = true;
+    if (this.addForm.valid) {
+      const { name, account, password, site } = this.addForm.value;
+      if (name && account && password && site) {
+        this.passwordService
+          .addPassword(name, account, password, site)
+          .subscribe({
+            next: (response) => this.router.navigateByUrl('/dashboard'),
+            error: (error) => (this.isLoading = false),
+          });
+      }
+    }
+  }
 }
