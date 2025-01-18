137a416c602889681591de33ed45cbc2b44da437
hyj <hyj@MacBook-Pro-de-hyj.local>
2025-01-18 23:46:19 +0100

    Agregar color y tamaño dinamico en el loader

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
   imports: [],
   templateUrl: './loader.component.html',
-  styleUrl: './loader.component.css'
+  styleUrl: './loader.component.css',
 })
 export class LoaderComponent {
-
+  @Input() color: string = '#f8f8f8';
+  @Input() size: number = 16;
 }
